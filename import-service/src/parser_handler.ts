70ca5f406b13710d8fe8dcb9980a382005576e1e
Mrinal Ghosh <Mrinal_Ghosh@epam.com>
2022-07-29 13:05:49 +0530

    [Task-5] : pasring logic updated to clear the upload dir

--- import-service/src/parser_handler.ts
+++ import-service/src/parser_handler.ts
@@ -3,35 +3,76 @@ import csv from "csv-parser";
 import { S3 } from "aws-sdk";
 const s3Client = new S3({ region: "us-east-1" });
 
-const BUCKET = process.env.BUCKET || "";
-
 export const importFileParser = async (event: S3CreateEvent) => {
-  event.Records.forEach((record) => {
-    const s3Stream = s3Client
-      .getObject({
-        Bucket: BUCKET,
-        Key: record.s3.object.key,
-      })
-      .createReadStream();
+  for (const record of event.Records) {
+    const { object, bucket } = record.s3;
+    const params = {
+      Bucket: bucket.name,
+      Key: object.key,
+    };
 
-    s3Stream
+    const s3Stream = s3Client.getObject(params).createReadStream();
+
+    const parsedResults = await parseStreamByCSVParser(s3Stream);
+    console.log("parsedResults", parsedResults);
+
+    const success = await copyFileToParsedDir(bucket, object);
+    if (success) {
+      await clearUploadedFile(bucket, object);
+    }
+  }
+};
+
+async function clearUploadedFile(
+  bucket: { name: string },
+  object: { key: string }
+) {
+  try {
+    const deleteRequest = {
+      Bucket: bucket.name,
+      Key: object.key,
+    };
+    await s3Client.deleteObject(deleteRequest).promise();
+  } catch (e) {
+    console.log("delete parsed error", e);
+  }
+}
+
+async function copyFileToParsedDir(
+  bucket: { name: string },
+  object: { key: string }
+) {
+  try {
+    const copyObjectRequest = {
+      Bucket: bucket.name,
+      CopySource: `${bucket.name}/${object.key}`,
+      Key: object.key.replace("uploaded/", "parsed/"),
+    };
+
+    const result = await s3Client.copyObject(copyObjectRequest).promise();
+
+    return !!result;
+  } catch (e) {
+    console.log("copy to parsed error", e);
+  }
+}
+
+async function parseStreamByCSVParser(s3stream: any) {
+  const parsedData = await new Promise((resolve, reject) => {
+    const results: any[] = [];
+    s3stream
       .pipe(csv())
-      .on("data", (data) => {
-        console.log(data);
+      .on("data", (data: any) => {
+        results.push(data);
       })
-      .on("end", async () => {
-        console.log(`Copy from ${BUCKET}/${record.s3.object.key}`);
-
-        await s3Client
-          .copyObject({
-            Bucket: BUCKET,
-            CopySource: `${BUCKET}/${record.s3.object.key}`,
-            Key: record.s3.object.key.replace("uploaded", "parsed"),
-          })
-          .promise();
-
-        console.log(`Processed into ${BUCKET}/${record.s3.object.key.replace("uploaded","parsed")}`
-        );
+      .on("error", (error: any) => {
+        reject(error);
+        console.log("parse error", error);
+      })
+      .on("end", () => {
+        resolve(results);
       });
   });
-};
+
+  return parsedData;
+}
