23b1b0def77057a52403d25d4cad229d3133ee99
Mrinal Ghosh <Mrinal_Ghosh@epam.com>
2022-07-29 12:42:28 +0530

    [Task-5] : importHandler and parserHandler created

--- import-service/src/import_handler.ts
+++ import-service/src/import_handler.ts
@@ -1,15 +1,24 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { generateResponse } from "./utils";
+import { S3 } from 'aws-sdk';
+const s3Client = new S3({ region: 'us-east-1' });
 
-export const importFileParser = async (event: APIGatewayEvent) => {
-  let pool;
-  try {
-    console.log(event);
-    return generateResponse(200, {
-      message: "Successfull",
-      data: [],
-    });
-  } catch (error: any) {
-    return generateResponse(500, { message: "Error", error: error.message });
-  }
-};
+export const importProductsFile = async (event: APIGatewayEvent) => {
+    try {
+      const fileName = event?.queryStringParameters?.name;
+      const s3FilePath = `uploaded/${fileName}.csv`;
+
+      const params = {
+        Bucket: process.env.BUCKET,
+        Key: s3FilePath,
+        Expires: 120,
+        ContentType: "text/csv",
+      };
+
+      const url = await s3Client.getSignedUrlPromise("putObject", params);
+
+      return generateResponse(200, {message: 'Successful', url});
+    } catch (error: any) {
+      return generateResponse(500, {message: 'Error', error: error.message});
+    }
+  };
--- import-service/src/parser_handler.ts
+++ import-service/src/parser_handler.ts
@@ -1,15 +1,37 @@
-import { APIGatewayEvent } from "aws-lambda";
-import { generateResponse } from "./utils";
+import { S3CreateEvent } from "aws-lambda";
+import csv from "csv-parser";
+import { S3 } from "aws-sdk";
+const s3Client = new S3({ region: "us-east-1" });
 
-export const importProductsFile = async (event: APIGatewayEvent) => {
-  let pool;
-  try {
-    console.log(event);
-    return generateResponse(200, {
-      message: "Successfull",
-      data: [],
-    });
-  } catch (error: any) {
-    return generateResponse(500, { message: "Error", error: error.message });
-  }
+const BUCKET = process.env.BUCKET || "";
+
+export const importFileParser = async (event: S3CreateEvent) => {
+  event.Records.forEach((record) => {
+    const s3Stream = s3Client
+      .getObject({
+        Bucket: BUCKET,
+        Key: record.s3.object.key,
+      })
+      .createReadStream();
+
+    s3Stream
+      .pipe(csv())
+      .on("data", (data) => {
+        console.log(data);
+      })
+      .on("end", async () => {
+        console.log(`Copy from ${BUCKET}/${record.s3.object.key}`);
+
+        await s3Client
+          .copyObject({
+            Bucket: BUCKET,
+            CopySource: `${BUCKET}/${record.s3.object.key}`,
+            Key: record.s3.object.key.replace("uploaded", "parsed"),
+          })
+          .promise();
+
+        console.log(`Processed into ${BUCKET}/${record.s3.object.key.replace("uploaded","parsed")}`
+        );
+      });
+  });
 };
